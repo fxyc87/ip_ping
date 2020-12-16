02a3c55bc3fdaa80f2f7c614e3a72e87bc135717
余超 <xa87@live.cn>
2020-12-16 14:00:57 +0800

    首次提交

---
+++ IP_PING/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿
+namespace IP_PING
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "起始IP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(173, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "结束IP";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 45);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "查找";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(318, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "线程数";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(14, 24);
+            this.maskedTextBox1.Mask = "000.000.000.000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.PromptChar = ' ';
+            this.maskedTextBox1.Size = new System.Drawing.Size(98, 21);
+            this.maskedTextBox1.TabIndex = 6;
+            this.maskedTextBox1.Text = "192254000000";
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Location = new System.Drawing.Point(175, 24);
+            this.maskedTextBox2.Mask = "000.000.000.000";
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.PromptChar = ' ';
+            this.maskedTextBox2.Size = new System.Drawing.Size(98, 21);
+            this.maskedTextBox2.TabIndex = 7;
+            this.maskedTextBox2.Text = "194255005004";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(320, 25);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(81, 21);
+            this.numericUpDown1.TabIndex = 8;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(12, 117);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(388, 304);
+            this.listBox1.TabIndex = 9;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 12;
+            this.listBox2.Location = new System.Drawing.Point(406, 117);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(342, 304);
+            this.listBox2.TabIndex = 10;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(429, 24);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(81, 21);
+            this.numericUpDown2.TabIndex = 11;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(427, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "超时(ms)";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(417, 66);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(63, 39);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "清空";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(761, 433);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.maskedTextBox2);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "IP批量PING";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ IP_PING/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+
+namespace IP_PING
+{
+    public partial class Form1 : Form
+    {
+        List<Thread> threads = new List<Thread>();
+        static List<List<byte[]>> ips = new List<List<byte[]>>();
+        static List<string> msg = new List<string>();
+        static List<int> jd = new List<int>();
+        static List<string> msg2 = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "停止")
+            {
+                threads.ForEach(n =>
+                {
+                    try
+                    {
+                        n.Abort();
+                    }
+                    catch
+                    {
+
+                    }
+                });
+                button1.Text = "查找";
+                return;
+            }
+            else
+            {
+                button1.Text = "停止";
+            }
+            int num = (int)numericUpDown1.Value;
+
+            var r1 = IPAddress.TryParse(maskedTextBox1.Text, out IPAddress ip1);
+            var r2 = IPAddress.TryParse(maskedTextBox2.Text, out IPAddress ip2);
+            if (r1 == false || r2 == false) {
+                MessageBox.Show("IP转换失败,无效IP地址！");
+                return;
+            }
+            for (int i = 0; i < ips.Count; i++)
+            {
+                ips[i].Clear();
+            }
+            ips.Clear();
+            for (int i = 0; i < num; i++)
+            {
+                ips.Add(new List<byte[]>());
+            }
+            
+
+            var s1 = ip1.GetAddressBytes().Select(n=>(int)n).ToArray();
+            var s2 = ip2.GetAddressBytes().Select(n => (int)n).ToArray();
+
+            //10组 IP分配任务列表
+            //169.254.0.0    169.254.1.0
+            int index = 0;
+            for (int i1 = s1[0]; i1 <= s2[0]; i1++)
+            {
+                for (int i2 = s1[1]; i2 <= s2[1]; i2++)
+                {
+                    for (int i3= s1[2]; i3 <= s2[2]; i3++)
+                    {
+                        for (int i4 = s1[3]; i4 <= s2[3]; i4++)
+                        {
+                            ips[index].Add(new byte[] { (byte)i1, (byte)i2, (byte)i3, (byte)i4 });
+                            index++;
+                            if (index >= num)
+                                index = 0;
+                            //System.Diagnostics.Debug.WriteLine(string.Join(",", new int[] { i1,i2,i3,i4}));
+                        }
+                    }
+                }
+            }
+
+            int[] index2 = new int[4];
+            bool[] exit = new bool[4];
+            listBox1.Items.Clear();
+            jd.Clear();
+            msg2.Clear();
+            threads.Clear();
+            for (int i = 0; i < num; i++) {
+                listBox1.Items.Add("线程" + i);
+                jd.Add(0);
+                msg2.Add("初始化中");
+                var thread = new Thread(Ping);
+                thread.Start(Tuple.Create<int, int>(i, (int)numericUpDown2.Value));
+                threads.Add(thread);
+            }
+        }
+        static void Ping(object arg) {
+            try
+            {
+                int index = 0;
+                var arg2 = (Tuple<int, int>)arg;
+                Ping ping = new Ping();
+
+                while (true)
+                {
+                    var ip = new IPAddress(ips[arg2.Item1][index]);
+                    var r = ping.Send(ip, arg2.Item2);
+                    if (r.Status == IPStatus.Success)
+                    {
+                        msg.Add("IP:" + r.Address.ToString() + " 耗时:" + r.RoundtripTime + "ms");
+                        //System.Diagnostics.Debug.WriteLine(r.Address.ToString() + " 耗时:" + r.RoundtripTime);
+                    }
+                    else
+                    {
+
+                    }
+                    msg2[arg2.Item1] = ip.ToString();
+                    index++;
+                    jd[arg2.Item1] = (int)((float)index / (float)ips[arg2.Item1].Count * 100.0f);
+                    if (index >= ips[arg2.Item1].Count)
+                    {
+                        return;
+                    }
+                }
+
+            }
+            catch (Exception e) { 
+                
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < listBox1.Items.Count; i++) {
+                listBox1.Items[i] = "线程" + i +" \t" +msg2[i] + " \t" + jd[i]+"%";
+            }
+            lock (msg)
+            {
+                if (msg.Count > 0)
+                {
+                    listBox2.Items.Add(msg.First());
+                    msg.RemoveAt(0);
+                }
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+        }
+    }
+}
